438223f4c65cfedccd45f1ea70c3ab990be85384
iyoup11 <your-iyoup11-guanfumingrui@gmail.com>
2025-03-06 18:44:07 +0800

    修复依赖或端口设置

--- app.py
+++ app.py
@@ -331,4 +331,4 @@ def index():
     return render_template('index.html', news_list=news_list, history=history, page=page, total_pages=total_pages, translations=t)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
